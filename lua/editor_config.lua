9315a097086f154a583363b4372329674e087f9b
GG.mp4 <viliusradz@gmail.com>
2025-05-07 14:02:27 +0300

    updated config

--- lua/editor_config.lua
+++ lua/editor_config.lua
@@ -2,3 +2,8 @@ vim.opt.tabstop = 2
 
 -- 24-bit color in nvim
 vim.opt.termguicolors = true
+
+-- vim.bo.tabstop = 4 -- size of a hard tabstop (ts).
+-- vim.bo.shiftwidth = 4 -- size of an indentation (sw).
+-- vim.bo.expandtab = false -- always uses spaces instead of tab characters (et).
+-- vim.bo.softtabstop = 4 -- number of spaces a <Tab> counts for. When 0, feature is off (sts).
